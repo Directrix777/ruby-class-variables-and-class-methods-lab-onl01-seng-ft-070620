78a8023398c3987053cf03e45ac43bd5791651dd
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-16 16:15:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,16 @@
 
 class Song
   @@songs = []
+  attr_accessor :name, :artist, :genre
 
+  def initialize(name, artist, genre)
+    self.name = name
+    self.artist = artist
+    self.genre = genre
+    @@songs.push(self)
+  end
+  
   def self.count
     @@songs.count
+  end
 end
